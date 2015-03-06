08718f98111500c06387621e0d26c74f493b724c
hannespetur <hannespetur@gmail.com>
2015-03-06 23:46:35 +0000

    test commit

--- empytree.py
+++ empytree.py
@@ -33,25 +33,25 @@ def search_for_mp3s():
 
 
 def move_to_correct_path(root,file_name):
-	# Get global files
-	global FILES_MOVED
-
 	file_name_with_path = os.path.join(root,file_name)
 	audiofile = eyed3.load(file_name_with_path)
 	new_path = args.output
 	if not audiofile.tag:
 		warnings.warn("No tag found in file: "+file_name_with_path+". Skipping file.", TagWarning)
-		return 0
+		return -1
 
 	audiofile.tag.artist = remove_banned_words(audiofile.tag.artist)
 	audiofile.tag.album = remove_banned_words(audiofile.tag.album)
 	audiofile.tag.title = remove_banned_words(audiofile.tag.title)
 	audiofile.tag.save()
 
-	## 
-	print levels
-	if audiofile.tag.artist:
+	##
+	try:
+		print levels
+	except:
+		print "derp"
 
+	if audiofile.tag.artist:
 		new_path = os.path.join(new_path, audiofile.tag.artist)
 	else:
 		warnings.warn("No artist tag found in file: "+file_name, Warning)
@@ -71,14 +71,10 @@ def move_to_correct_path(root,file_name):
 		if args.verbose:
 			print "Moved "+file_name+" to "+new_path
 
-	FILES_MOVED += 1
 	return 0
 
 ## The Main function
 if __name__ == '__main__':
-	global FILES_MOVED
-	FILES_MOVED = 0
-
 	# Create a argument parser
 	parser = argparse.ArgumentParser(description='Organize MP3 using ID3 tags.')
 	## Hint: add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
@@ -105,7 +101,7 @@ if __name__ == '__main__':
 		args.output = args.output[2:]
 
 	try:
-		json_data = open(args.config,'r')
+		json_data = open('config.json','r')
 		global data, levels
 		data = json.load(json_data)
 		levels = len(data["treeFormat"])
@@ -125,10 +121,12 @@ if __name__ == '__main__':
 		print "Output directory is: "+args.output
 		print "============================="
 
+	files_moved = 0
 	for root,mp3file in search_for_mp3s():
 		# if args.verbose:
 		# 	print "Trying to move file "+mp3file
-		move_to_correct_path(root,mp3file)
+		if not move_to_correct_path(root,mp3file):
+			files_moved += 1
 
 	if args.verbose:
-		print "Empytree finished successfully! Total files moved:",FILES_MOVED
+		print "Empytree finished successfully! Total files moved:",files_moved
